6c3676d4c1e2af5e9c567669fce1556141c2f41d
Noelle Daley <adriannenoelle@gmail.com>
2015-09-14 20:15:20 -0700

    first few lines working, need to try different separators

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import urllib2
 response = urllib2.urlopen('http://pythonforbeginners.com/')
 html = response.read()
 
-html_list = html.split()
+html_list = html.split(">")
 
-print html_list[1]
+print html_list[0], html_list[1], html_list[2]
 
 # print html
