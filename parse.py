0a8f0f542753ac4bf373621d10e9eba4c4b32e35
Noelle Daley <adriannenoelle@gmail.com>
2015-09-15 15:04:39 -0700

    html is missing some inner divs

--- parse.py
+++ parse.py
@@ -1,15 +1,19 @@
-import urllib2
+import requests
 import lxml.html
-input_url = 'http://noelledaley.github.io/'
+input_url = 'http://docs.python-guide.org/en/latest/scenarios/scrape/'
 
-# Fetch HTML of input url and store as string
-response = urllib2.urlopen(input_url)
-html = response.read()
+# Fetch HTML of input url and store as unicode
+page = requests.get(input_url)
 
-# Convert HTML string to Tree
-tree = lxml.html.fromstring(html)
+# Convert HTML unicode to Tree
+tree = lxml.html.fromstring(page.text)
 
 def count_elements(tree):
     """Given an HTML Tree, count number of elements."""
+    count = {}
+
     for element in tree.iter():
-        print element.tag
+        print element
+        count[element.tag] = count.setdefault(element.tag, 0) + 1
+
+    return count
