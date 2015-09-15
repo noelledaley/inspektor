b34e0e9f32e0f3464b759ffd55a0d10ecbe71757
Noelle Daley <adriannenoelle@gmail.com>
2015-09-14 20:18:31 -0700

    updated gitignore

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 import urllib2
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ['FLASK_TOKEN']
 
 response = urllib2.urlopen('http://pythonforbeginners.com/')
 html = response.read()
@@ -8,3 +13,5 @@ html_list = html.split(">")
 print html_list[0], html_list[1], html_list[2]
 
 # print html
+
+if name if __name__ == '__main__':
