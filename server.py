f7266a76d93948ea61b736041654b47b345c9820
Noelle Daley <adriannenoelle@gmail.com>
2015-09-17 12:46:52 -0700

    added comments

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 from jinja2 import StrictUndefined
 from parse import build_element_histogram, encode_html, add_spans
 import os
@@ -31,6 +31,12 @@ def fetch_html():
 
     # Remove HTML entities from html to display on page
     raw_html = encode_html(html)
+
+    # Add spans to each element so jQuery can select and apply highlight class
+    # TODO: the span classes currently added have extra information on them.
+        # Need to figure out how to strip so only element name is added.
+        # e.g. <span class='div id="icons"'><code>&lt;div id="icons"&gt;</code></span>
+        # >>> <span class="div"><code>&lt;div id="icons"&gt;</code></span>
     span_html = add_spans(raw_html)
 
     # Convert HTML unicode to Tree
@@ -39,7 +45,7 @@ def fetch_html():
     # Get histogram of element frequencies
     frequency = build_element_histogram(tree)
 
-    return render_template('results.html', frequency=frequency, raw_html=span_html)
+    return render_template('results.html', frequency=frequency, raw_html=html)
 
 
 if __name__ == '__main__':
