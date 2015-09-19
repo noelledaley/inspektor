d2d62d23339a74e30f3b23df939ef875cf9ba168
Noelle Daley <adriannenoelle@gmail.com>
2015-09-19 12:05:15 -0700

    added comments

--- static/highlight-code.js
+++ static/highlight-code.js
@@ -1,11 +1,15 @@
 
 $(document).ready(function () {
   $('.element').on('click', function() {
+    // Add an event listener to each element tag in Summary table
 
     var elementTag = 'my-' + this.innerHTML;
     $('.' + elementTag).toggleClass('highlight');
+
+    // Toggle entire table rows 
     $(this).toggleClass('highlight');
-    console.log('toggling class');
+    $(this).next().toggleClass('highlight');
+
   });
 
 });
