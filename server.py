f877acab239ea1b105f95f3c2f40f99c21f88643
Noelle Daley <adriannenoelle@gmail.com>
2015-09-19 12:48:08 -0700

    setting up server for heroku

--- server.py
+++ server.py
@@ -61,5 +61,7 @@ def show_prototype():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
